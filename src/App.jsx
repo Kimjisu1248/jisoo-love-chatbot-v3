b995c25391cdb407030bb70a5519685d179e8ea0
Kimjisu1248 <kjs021123@naver.com>
2025-05-23 15:21:31 +0900

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ChatRoom from "./components/ChatRoom";
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>지수공주님 연애 챗봇 V3 💖</h1>
+      <ChatRoom />
+    </div>
+  );
+}
