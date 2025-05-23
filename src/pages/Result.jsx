177da298fa11a2f720bb2823bfd8dab6a1c3e857
Kimjisu1248 <kjs021123@naver.com>
2025-05-23 15:28:56 +0900

    Add files via upload

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,1 +1,10 @@
+import React from "react";
 
+export default function Result() {
+  return (
+    <div>
+      <h2>지수공주님의 이상형 분석 결과!</h2>
+      <p>MBTI: ENFP<br/>말투: 장난기+다정함<br/>캐릭터: 민정오빠</p>
+    </div>
+  );
+}
